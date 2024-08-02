57e6817610912e4cb0e0d441565f6baf62e1bfde
Jiří Merenda <j.merenda@seznam.cz>
2024-08-02 19:52:54 +0200

    Add TimestampableTrait - createdAt, updatedAt

---
+++ src/Entity/Trait/TimestampableTrait.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity\Trait;
+
+use DateTime;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\HasLifecycleCallbacks]
+trait TimestableTimestampableTrait
+{
+    /**
+     * @var DateTime datum vytvoření stránky
+     */
+    #[ORM\Column(type: 'datetime', nullable: true)]
+    private $createdAt;
+
+    /**
+     * @var DateTime datum poslední úpravy stránky
+     */
+    #[ORM\Column(type: 'datetime', nullable: true)]
+    private $updatedAt;
+
+    
+    #[ORM\PrePersist]
+    #[ORM\PreUpdate]
+    public function updatedTimestamps(): void
+    {
+        $dateNow = new DateTime('now');
+
+        $this->updatedAt = $dateNow;
+
+        if ($this->createdAt == null) {
+            $this->setCreatedAt($dateNow);
+        }
+    }
+
+    public function getCreatedAt(): ?DateTime
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @param DateTime|null $createdAt
+     */
+    public function setCreatedAt(DateTime $createdAt): void
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    public function getUpdatedAt(): ?DateTime
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * @param DateTime|null $updatedAt
+     */
+    public function setUpdatedAt(DateTime $updatedAt): void
+    {
+        $this->updatedAt = $updatedAt;
+    }
+}
