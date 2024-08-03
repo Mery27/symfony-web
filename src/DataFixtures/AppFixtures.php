7e00823cb17694f71f2204457ef979666b01aa58
Mery27 <j.merenda@seznam.cz>
2024-08-03 14:46:41 +0200

    Update datafixture for Page

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,9 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Seo;
+use App\Entity\Page;
+use App\Entity\OGTags;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,9 +12,36 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        
 
-        $manager->flush();
+        for ($i = 1; $i < 20; $i++) {
+
+            $page = new Page();
+            $seo = new Seo();
+            $ogTags = new OGTags();
+
+            $page->setTitle("Test " . (string) $i);
+            $page->setUrl("test-" . (string) $i);
+            $page->setShortBody('Short body');
+            $page->setBody('Long text');
+            $seo->setTitle('Seo title' . (string) $i);
+            $seo->setHideInSitemap(true);
+            $seo->setAuthor('localhost');
+            $seo->setRobots('noidex, nofollow');
+            $seo->setKeywords('symfony, locahost, development php');
+            $seo->setDescription('Seo description for page ' . (string) $i);
+            $page->setSeo($seo);
+            $ogTags->setTitle('OG - title ' . (string) $i);
+            $ogTags->setType('article');
+            $ogTags->setLocale('cs_CZ');
+            $ogTags->setSiteName('localhost');
+            $page->setOgTags($ogTags);
+            $page->setIsPublished(true);
+
+            $manager->persist($seo);
+            $manager->persist($ogTags);
+            $manager->persist($page);
+            $manager->flush();
+        }
     }
 }
