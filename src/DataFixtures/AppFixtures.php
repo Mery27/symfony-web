ebb12b844c73607e58364c08452f71be2dd8ffd6
Jiří Merenda <j.merenda@seznam.cz>
2024-08-05 19:18:03 +0200

    Update DataFixture for Page

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -7,12 +7,19 @@ use App\Entity\Page;
 use App\Entity\OGTags;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use joshtronic\LoremIpsum;
 
 class AppFixtures extends Fixture
 {
+
+    // public function __construct(
+    //     private LoremIpsum $lorem,
+    // ){
+    // }
+
     public function load(ObjectManager $manager): void
     {
-        
+        $lorem = new LoremIpsum();
 
         for ($i = 1; $i < 20; $i++) {
 
@@ -20,21 +27,32 @@ class AppFixtures extends Fixture
             $seo = new Seo();
             $ogTags = new OGTags();
 
-            $page->setTitle("Test " . (string) $i);
-            $page->setUrl("test-" . (string) $i);
-            $page->setShortBody('Short body');
-            $page->setBody('Long text');
-            $seo->setTitle('Seo title' . (string) $i);
-            $seo->setHideInSitemap(true);
+            $page->setTitle("Page " . (string) $i);
+            $page->setUrl("page-" . (string) $i);
+            $page->setShortBody($lorem->paragraph('p'));
+            $page->setBody($lorem->paragraphs(5, 'p'));
+
+            $date = new \DateTime('now');
+            $date->modify("+{$i} hour");
+            $date = new \DateTimeImmutable($date->format('Y-m-d H:i:s'));
+            $page->setCreatedAt($date);
+
+            $seo->setTitle('Seo page' . (string) $i);
+            $seo->setDescription($this->trimParagraph(150));
+            $seo->setKeywords('symfony, locahost, development php');
             $seo->setAuthor('localhost');
             $seo->setRobots('noidex, nofollow');
-            $seo->setKeywords('symfony, locahost, development php');
-            $seo->setDescription('Seo description for page ' . (string) $i);
-            $page->setSeo($seo);
-            $ogTags->setTitle('OG - title ' . (string) $i);
+            $seo->setHideInSitemap(true);
+            
+            $ogTags->setTitle('OG - page ' . (string) $i);
+            $ogTags->setDescription($this->trimParagraph(200));
             $ogTags->setType('article');
+            $ogTags->setUrl("page-" . (string) $i);
             $ogTags->setLocale('cs_CZ');
-            $ogTags->setSiteName('localhost');
+            $ogTags->setSiteName('Symfony');
+            $ogTags->setImage('https://picsum.photos/id/684/600/400');
+            
+            $page->setSeo($seo);
             $page->setOgTags($ogTags);
             $page->setIsPublished(true);
 
@@ -43,5 +61,11 @@ class AppFixtures extends Fixture
             $manager->persist($page);
             $manager->flush();
         }
+    }
+
+    private function trimParagraph(int $length = 240, bool $htmlTagP = false) {
+        $lorem = new LoremIpsum();
+        
+        return  ($htmlTagP ? '<p>' : '') . substr($lorem->paragraph(), 0, $length) . ($htmlTagP ? '</p>' : '');
     }
 }
